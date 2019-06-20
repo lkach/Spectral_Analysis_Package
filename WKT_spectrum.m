8e2b88124f3961f6592c77814b8042fc18d3e292
Luke Kachelein <lukachelein@gmail.com>
2019-06-20 13:35:13 +0200

    Update WKT_spectrum.m

--- WKT_spectrum.m
+++ WKT_spectrum.m
@@ -1,9 +1,34 @@
-% X, maxlag, 
-function [spec, f_spec] = WKT_spectrum(X,maxlag)
-% Make spectrum, or use the one given if one is given:
+% [Spec, f_spec] = WKT_spectrum(X,maxlag)
+%
+% Estimates the power spectrum using the Wiener–Khinchin theorem (Fourier
+% transform of the autocovariance), as opposed to Welch's method (averaged
+% periodigrams).
+%
+% IN:   X = Nx1 vector time series. May contain NaN's and/or Inf's, but
+%           data must be evenly spaced.
+% IN:   maxlags = Scalar, the maximum lag (multiple of the time step)
+%           considered by the internally calculated autocovariance.
+% IN:   WindowMethod = (Optional) String, the function used to build the
+%           window by which the autocovariance is multiplied. The default
+%           is "hann". For no adjustment to the autocovariance, use
+%           "rectwin".
+%
+% OUT:  Spec = The output power spectrum, which has units of X^2 / freq.
+%           Multiply by f_spec(1) to get variance.
+% OUT:  f_spec = The frequencies corresponding to the elements of "Spec".
+%           This has units of 1/[units of time step], where
+%           1 time step = 1 time unit (e.g. if dt = 1 hr, f_spec is in
+%           1/hr; likewise, if dt = 3 seconds, f_spec is in 1/(3 sec);
+%           convert accordingly)
+%
+
+function [Spec, f_spec] = WKT_spectrum(X,maxlag,varargin)
+
+X(isinf(X)) = nan;
+
 N_x = length(X);
 X = X - nanmean(X);
-% if ...
+
 autocov_x = zeros(1 + maxlag,1); % the autocovariance
 % autocov_std_x = zeros(1 + maxlag,1); % the std of the points that went into each element in pre-shaped "autocov_x" (starting with zero-lag)
 % n_autocov_x = zeros(1 + maxlag,2); % how many pairs were finite and how many pairs could have been finite if perfect
@@ -13,54 +38,61 @@ for i=1:(1 + maxlag) % lag = (i-1)*dt
         autocov_x_i(j) = X(j)*X(j+i-1);
     end
     autocov_x(i) = nanmean(autocov_x_i); % autocov
-%     autocov_std_x(i) = nanstd(autocov_x_i); % std of points that made each element of autocov
-%     n_autocov_x(i,1) = sum(isfinite(autocov_x_i)); % how many pairs were finite
-%     n_autocov_x(i,2) = N_x - i + 1; % how many pairs could have been finite if perfect
+    %     autocov_std_x(i) = nanstd(autocov_x_i); % std of points that made each element of autocov
+    %     n_autocov_x(i,1) = sum(isfinite(autocov_x_i)); % how many pairs were finite
+    %     n_autocov_x(i,2) = N_x - i + 1; % how many pairs could have been finite if perfect
 end
 % NOTE: "autocov_std_x" and "n_autocov_x" are unused, and are
 % retained here for troubleshooting and possible future inclusion.
 autocov_x = [autocov_x;flip(autocov_x(2:(end-1)))]; % autocov_x = [autocov_x;0;flip(autocov_x(2:end))];
-Window = fftshift(hann(length(autocov_x),'periodic'));
-spec = fft(autocov_x.*Window); % Window applied for smoothing
-figure; subplot(211); plot(Window,'.-'); subplot(212); plot(autocov_x,'.-')%$debugging
-spec = spec/length(spec); % Satisfy Parseval's Theorem
-if var(real(spec)) < 10000*var(imag(spec)) % make sure that the autocovariance is periodic and thus that its fft is real
-    warning(['Imaginary part [removed] of fft(autocov_x) was ',num2str(var(imag(spec))/var(real(spec)))],' times that of the real part.')
+
+if nargin == 2
+    Window = fftshift(hann(length(autocov_x),'periodic'));
+elseif nargin == 3
+    Window = eval(['fftshift(',varargin{1},'(length(autocov_x),''periodic''));']);
+else
+    error('Incorrect number of inputs; please read documentation.')
 end
-spec = real(spec);
-if sum(spec < 0) % if the WKT spectrum is negative at any frequency
+
+Spec = fft(autocov_x.*Window); % Window applied for smoothing
+
+% figure; subplot(211); plot(Window,'.-'); subplot(212); plot(autocov_x,'.-') % uncomment for debugging
+
+Spec = Spec/length(Spec); % Satisfy Parseval's Theorem
+if var(real(Spec)) < 10000*var(imag(Spec)) % make sure that the autocovariance is periodic and thus that its fft is real
+    warning(['Imaginary part [removed] of fft(autocov_x) was ',num2str(var(imag(Spec))/var(real(Spec)))],' times that of the real part.')
+end
+Spec = real(Spec);
+if sum(Spec < 0) % if the WKT spectrum is negative at any frequency
     warning(['The internally calculated spectrum (using the WKT) results',...
         ' in a partly negative spectrum.']);
-    % error('See warning.') % Only if we don't want this as a possibility.
+    % error('See warning.')
+    % % ^^^ Uncomment if the possibility of negative spectrum is strongly undesired.
 else
 end
-% Fold the spectrum in on itself du to symmetry:
-if mod(length(spec),2) % odd length
-    spec = spec(1:ceil(length(spec)/2));%%%
-elseif ~mod(length(spec),2) % even length
-    spec = spec(1:ceil(length(spec)/2 + 0.5));%%%
+% Fold the spectrum in on itself to give it symmetry:
+if mod(length(Spec),2) % odd length
+    Spec = Spec(1:ceil(length(Spec)/2));
+elseif ~mod(length(Spec),2) % even length
+    Spec = Spec(1:ceil(length(Spec)/2 + 0.5));
 end
 f_Ny = 1/2; % "T" will be evenly-spaced if the user follows the directions in the documentation
-    % Eliminated because it shifted and aligned frequencies incorrectly:
-    % df_cov = f_Ny/length(spec);
-    % f_spec = [df_cov:df_cov:f_Ny]';
-    % spec(2:end) = 2*spec(2:end);
-spec(2:end) = 2*spec(2:end);
-spec = spec(2:end);
-df_cov = f_Ny/length(spec);
-f_spec = [df_cov:df_cov:f_Ny]';
-
+Spec(2:end) = 2*Spec(2:end);
+Spec = Spec(2:end);
+df_cov = f_Ny/length(Spec);
+f_spec = (df_cov:df_cov:f_Ny)';
+Spec = Spec/df_cov; % Make this a proper power spectrum
 end
 
-%% Test Case
+%% Example
 
 % T = 0:999;
 % X = 3*sin(2*pi*T/50) + randn(size(T)) + 10;
 % 
 % [WelchS, WelchF, ~] = nanspectrum_LK(X, 1, 'unit', 5, '.-', 0, 0, 'hanning');
 % [wktS, wktF] = WKT_spectrum(X,length(T)/10);
-% %%
+% 
 % figure
 % plot(wktF,wktS,'.-'); hold on
-% plot(WelchF,WelchS*(5/(T(end)-T(1))),'.-')
-% % set(gca,'yscale','log')
+% plot(WelchF,WelchS,'.-')
+% set(gca,'yscale','log')
