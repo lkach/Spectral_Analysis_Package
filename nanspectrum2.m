edfc9cf80351153bcde7927319b60e681c261722
Luke Kachelein <lukachelein@gmail.com>
2023-12-29 17:04:08 -0800

    Update nanspectrum2.m
    Added "help" output.

--- nanspectrum2.m
+++ nanspectrum2.m
@@ -1,5 +1,45 @@
-% [SPEC, F12, err] = nanspectrum2(DATA, [D1  D2], [SEG1 SEG2], {UNITS1, UNITS2}, PLOT_BOOLEAN, WINDOWMETHOD)
-
+% [SPEC, F12, err] = NANSPECTRUM2(DATA, [D1  D2], [SEG1 SEG2], {UNITS1, UNITS2}, PLOT_BOOLEAN, WINDOWMETHOD)
+% 
+% Two-dimensional power spectrum estimation. Arbitrary dimensions for the
+% two inputs (e.g. space-time or space-space).
+% 
+% -------------------------------------------------------------------------
+% REQUIRED INPUTS:
+%   DATA = Data matrix, size nd by md
+%   DD   = 2-element vector where:
+%          DD(1) = step length in the horizontal dimension of DATA
+%          DD(2) = step length in the vertical dimension of DATA
+%   SEGS = 2-element vector where:
+%          SEGS(1) = number of segments in the horizontal dimension of DATA
+%          SEGS(2) = number of segments in the vertical dimension of DATA
+% 
+% OPTIONAL INPUTS:
+%   UNITS = 2-element cell where:
+%          UNITS{1} = physical units of the horizontal dimension of DATA
+%          UNITS{2} = physical units of the vertical dimension of DATA
+%   PLOT_BOOLEAN = true or false, decides whether or not to plot (default false)
+%   WINDOWMETHOD = string, name of the windowing function applied to
+%          segments in both dimensions (default 'hanning')
+% 
+% OPTIONAL INPUT NAME-VALUE PAIRS:
+%   'Detrend'   default true, decides if each segment should have a planar
+%               fit removed (true) or not (false)
+%   'Overlap'   default true, decides if overlapping segments should be
+%               used
+%   'Colormap'  default 'parula', the colormap used for plots (only used if
+%               PLOT_BOOLEAN == true)
+% -------------------------------------------------------------------------
+% OUTPUTS:
+%   SPEC = Power spectrum, size nd/SEGS(2) by md/SEGS(1), units of
+%          [data_units]^2 * [UNITS{1}] * [UNITS{2}]
+%   F12  = 3-dimensional array, size nd/SEGS(2) by md/SEGS(1) by 2, where
+%          F12(:,:,1) = frequencies corresponding to the horizontal
+%                       dimension of DATA
+%          F12(:,:,2) = frequencies corresponding to the vertical
+%                       dimension of DATA
+%   err  = (OPTIONAL) error ratio, where err(1) = 1 and err(2) > 1.
+%          Calculated using chi2inv (see function for more information).
+% -------------------------------------------------------------------------
 
 function [SPEC, F12, varargout] = nanspectrum2(DATA, DD, SEGMENTS, varargin)
 %% Variables defined
@@ -54,8 +94,11 @@ end
 N1 = size(DATA,1); % Y's
 N2 = size(DATA,2); % X's
 
-D1 = DD(1); D2 = DD(2);
-SEG1 = SEGMENTS(1); SEG2 = SEGMENTS(2);
+% Confusingly, this flips the expected order of the inputs:
+D1 = DD(2); % delta in the Y direction
+D2 = DD(1); % delta in the X direction
+SEG1 = SEGMENTS(2); % segments in the Y direction
+SEG2 = SEGMENTS(1); % segments in the X direction
 
 error_Msg = ['Make sure that the data can actually be divided into the given number of windows.' char(13) ...
         'As it is given, the data has ' num2str(N1) 'data points in the vertical direction and ' char(13) ...
@@ -90,15 +133,18 @@ for ii = 1:[SEG1*SEG2]
     %  min([1:(N2/SEG2)] + (Ixs(ii)-1)*N2/SEG2), max([1:(N2/SEG2)] + (Ixs(ii)-1)*N2/SEG2)]
 end
 % % % Overlapping part:
-Iys = reshape(mod(0:[[[SEG1-1]*[SEG2-1]]-1],SEG1-1)+1,[SEG1-1,SEG2-1]); % Index y segments
-Ixs = reshape(mod(0:[[[SEG1-1]*[SEG2-1]]-1],SEG2-1)+1,[SEG2-1,SEG1-1])'; % Index x segments
-Iys = Iys(:); Ixs = Ixs(:);
-for ii = 1:[[SEG1-1]*[SEG2-1]]
-    DATA_reshape(:,:,ii+[SEG1*SEG2]) = DATA([1:(N1/SEG1)] + (Iys(ii)-1)*N1/SEG1 + floor(0.5*[N1/SEG1]+1)-1, ...
-                                            [1:(N2/SEG2)] + (Ixs(ii)-1)*N2/SEG2 + floor(0.5*[N2/SEG2]+1)-1);
-    % % % For index verification:
-    % [min([1:(N1/SEG1)] + (Iys(ii)-1)*N1/SEG1 + floor(0.5*[N1/SEG1]+1)-1), max([1:(N1/SEG1)] + (Iys(ii)-1)*N1/SEG1 + floor(0.5*[N1/SEG1]+1)-1);...
-    %  min([1:(N2/SEG2)] + (Ixs(ii)-1)*N2/SEG2 + floor(0.5*[N2/SEG2]+1)-1), max([1:(N2/SEG2)] + (Ixs(ii)-1)*N2/SEG2 + floor(0.5*[N2/SEG2]+1)-1)]
+if Overlap
+    Iys = reshape(mod(0:[[[SEG1-1]*[SEG2-1]]-1],SEG1-1)+1,[SEG1-1,SEG2-1]); % Index y segments
+    Ixs = reshape(mod(0:[[[SEG1-1]*[SEG2-1]]-1],SEG2-1)+1,[SEG2-1,SEG1-1])'; % Index x segments
+    Iys = Iys(:); Ixs = Ixs(:);
+    for ii = 1:[[SEG1-1]*[SEG2-1]]
+        DATA_reshape(:,:,ii+[SEG1*SEG2]) = DATA([1:(N1/SEG1)] + (Iys(ii)-1)*N1/SEG1 + floor(0.5*[N1/SEG1]+1)-1, ...
+                                                [1:(N2/SEG2)] + (Ixs(ii)-1)*N2/SEG2 + floor(0.5*[N2/SEG2]+1)-1);
+        % % % For index verification:
+        % [min([1:(N1/SEG1)] + (Iys(ii)-1)*N1/SEG1 + floor(0.5*[N1/SEG1]+1)-1), max([1:(N1/SEG1)] + (Iys(ii)-1)*N1/SEG1 + floor(0.5*[N1/SEG1]+1)-1);...
+        %  min([1:(N2/SEG2)] + (Ixs(ii)-1)*N2/SEG2 + floor(0.5*[N2/SEG2]+1)-1), max([1:(N2/SEG2)] + (Ixs(ii)-1)*N2/SEG2 + floor(0.5*[N2/SEG2]+1)-1)]
+    end
+else
 end
 
 %% Segment-by-segment interpolate over NaN's using a plane fit (real and imaginary separately)
@@ -144,7 +190,6 @@ for seg_i = 1:size(DATA_reshape,3)
 end
 
 SPEC = sum(abs(fftDATA).^2, 3)/size(fftDATA,3);
-% SPEC = SPEC(2:end,2:end);
 SPEC = fftshift(SPEC,1);
 SPEC = fftshift(SPEC,2);
 
@@ -153,11 +198,14 @@ SPEC = fftshift(SPEC,2);
 F1 = [-1/2]:[1/[N1/SEG1]]:[1/2]; F1 = F1/D1;
 F2 = [-1/2]:[1/[N2/SEG2]]:[1/2]; F2 = F2/D2;
 
-[F1,F2] = meshgrid(F1(1:[end-1]),F2(1:[end-1]));
+% [F2,F1] = meshgrid(F2(1:[end-1]),F1(1:[end-1]));
+[F2,F1] = meshgrid(F2(1:[end-1]),F1(2:end));
 
-F1 = F1'; F2 = F2';
+F1 = flip(F1);
 
-F12 = cat(3,F1,F2);
+F12 = cat(3,F2,F1);
+
+figure;subplot(121);imagesc(F12(:,:,1));subplot(122);imagesc(F12(:,:,2))%$
 
 %% Normalize:
 
@@ -166,21 +214,15 @@ SPEC = norm_factor*SPEC*D1*D2/[[N1/SEG1]*[N2/SEG2]];
 %% Plot (if requested)
 
 if PLOT_BOOLEAN
-    % figure
-    % imagesc(unique(F1),unique(F2),log10(SPEC))
-    % title('imagesc')
-    % clim([prctile(log10(SPEC(:)),1) max(log10(SPEC(:)))])
-
+    % whos F1 F2 SPEC
     figure
     pcolor_centered(F2,F1,log10(SPEC)); shading flat
-    title(['Power spectrum units: [(time\_series\_units)^2 (cycles per ' UNITS{1} ')^{-1} (cycles per ' UNITS{2} ')^{-1}]'])
     clim([prctile(log10(SPEC(:)),1) max(log10(SPEC(:)))])
     xlabel(['Cycles per ' UNITS{1}])
     ylabel(['Cycles per ' UNITS{2}])
     colormap(Colormap)
     c = colorbar;
     c.YLabel.String = 'log_{10}S';
-    % ax = gca; ax.YDir = 'normal'; % for imagesc
 
     % % % Data variance:
     disp('Data variance:')
@@ -241,3 +283,33 @@ D_ = [D , nan(size(D,1),1) ; ...
 PC = pcolor(X_ - dx/2, Y_ - dy/2, D_);
 end
 end
+%% Example
+
+% close all
+% 
+% dT = 1;
+% TT = 0:dT:999;
+% T_wave = 50; Omega_wave = 2*pi/T_wave;
+% 
+% dX = 0.1;
+% XX = [0:dX:99.9]';
+% L_wave = 5; K_wave = 2*pi/L_wave;
+% PHASE_SPEED = Omega_wave/K_wave;
+% 
+% WAVES = real( exp(1i*[K_wave*XX - Omega_wave*TT]) );
+% WAVES = WAVES + randn(size(WAVES))*std(WAVES(:),'omitnan')*0.05;
+% 
+% % % % Animate:
+% % figure
+% % for ti = 1:length(TT)
+% %     plot(XX,WAVES(:,ti),'.-')
+% %     pause(0.05)
+% % end
+% 
+% figure
+% imagesc(WAVES); colorbar
+% 
+% tic
+% [SPEC,FF] = nanspectrum2(WAVES, [dT dX], [5 2], {'second','meter'}, true, 'hanning', ...
+%     'Colormap','parula' , 'Overlap',true , 'Detrend',false);
+% toc
